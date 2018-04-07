c2592f08eae204fc35f4982eb704b801a19836b5
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-06 21:40:44 -0400

    Basic layout of the VueGist with props defined

---
+++ src/components/VueGist.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <app-gist-core :gist-div="gistData"/>
+  </div>
+</template>
+
+<script>
+import VueGistCore from './VueGistCore.vue'
+export default {
+  components: {
+      appGistCore: VueGistCore,
+  },
+  props: {
+      gistId: {
+          type: String,
+          required: true
+      },
+      file: {
+          type: String,
+          required: false,
+          default: ''
+      }
+  },
+  data() {
+      return {
+          gistData: 'loading...',
+      }
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://assets-cdn.github.com/assets/gist-embed-1baaff35daab552f019ad459494450f1.css');
+</style>
