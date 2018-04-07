025091956d8e5f49f3c2de0c02720c37168ee250
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-06 21:32:55 -0400

    Basic setup of gist core functionality

---
+++ src/components/VueGistCore.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p v-html="gistDiv" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+      gistDiv: {
+          type: String,
+          required: true
+      }
+  },
+}
+</script>
+<style scoped>
+@import url('https://assets-cdn.github.com/assets/gist-embed-1baaff35daab552f019ad459494450f1.css');
+</style>
