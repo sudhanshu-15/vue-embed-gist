25789d93391340bed41d7dc79aa5efdf175eeaf8
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-20 04:22:25 -0400

    Updates

---
+++ App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div 
+    id="app" 
+    class="div-style">
+    <h2>vue-embed-gist example</h2>
+    <br>
+    <h3>With gistId</h3>
+    <vue-embed-gist 
+      gist-id="8bab656a910829ab9c32d7700c570be6"/>
+    <br>
+    <h3>With gistId and file</h3>
+    <vue-embed-gist 
+      gist-id="8bab656a910829ab9c32d7700c570be6" 
+      file="index.js"/>
+  </div>
+</template>
+
+<script>
+import VueEmbedGist from '../src'
+
+export default {
+  components: {
+    VueEmbedGist
+  }
+}
+</script>
+
+<style scoped>
+.div-style {
+  width: 50%;
+  align-self: center;
+  align-content:center;
+  text-align: center;
+  margin: 0 auto; 
+}
+</style>
