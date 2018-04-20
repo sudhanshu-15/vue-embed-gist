1dc10235015fd6f034cb5321a5c34caf61dc320f
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-20 05:02:51 -0400

    Added configurations for gh-pages

--- example/App.vue
+++ example/App.vue
@@ -2,6 +2,18 @@
   <div 
     id="app" 
     class="div-style">
+    <h2>vue-embed-gist usage</h2>
+    <br>
+    <h3>Install vue-embed-gist using npm</h3>
+    <vue-embed-gist 
+      gist-id="8bab656a910829ab9c32d7700c570be6"
+      file="npminstall.sh"/>
+    <br>
+    <h3>Usage</h3>
+    <vue-embed-gist 
+      gist-id="8bab656a910829ab9c32d7700c570be6" 
+      file="usage.vue"/>
+    <br>
     <h2>vue-embed-gist example</h2>
     <br>
     <h3>With gistId</h3>
