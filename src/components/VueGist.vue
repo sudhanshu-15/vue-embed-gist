88d476f636b282076de009c24df9b766d1b42b10
Sudhanshu Siddh <ssiddh@iu.edu>
2019-08-22 00:54:28 -0700

    Updated outdated css path

--- src/components/VueGist.vue
+++ src/components/VueGist.vue
@@ -1,61 +1,61 @@
 <template>
   <div>
-    <app-gist-core :gist-div="gistData"/>
+    <app-gist-core :gist-div="gistData" />
   </div>
 </template>
 
 <script>
-var gistUrl = 'https://gist.github.com/'
-var data = {}
+var gistUrl = "https://gist.github.com/";
+var data = {};
 
-import VueGistCore from './VueGistCore.vue'
-import $ from 'jquery'
+import VueGistCore from "./VueGistCore.vue";
+import $ from "jquery";
 export default {
   components: {
-      appGistCore: VueGistCore,
+    appGistCore: VueGistCore
   },
   props: {
-      gistId: {
-          type: String,
-          required: true
-      },
-      file: {
-          type: String,
-          required: false,
-          default: ''
-      }
+    gistId: {
+      type: String,
+      required: true
+    },
+    file: {
+      type: String,
+      required: false,
+      default: ""
+    }
   },
   data() {
-      return {
-          gistData: 'loading...',
-      }
+    return {
+      gistData: "loading..."
+    };
   },
-  created () {
-      this.getGistData(this.gistId)
+  created() {
+    this.getGistData(this.gistId);
   },
   methods: {
-      getGistData (gistId) {
-          var self = this;
-          if(this.file.length > 0){
-              data.file = this.file;
-          } 
-          $.ajax({
-              url: gistUrl + gistId + '.json',
-              data: data,
-              dataType: 'jsonp',
-              timeout: 20000,
-              success: function(response) {
-                  self.gistData = response.div;
-              },
-              error: function(response) {
-                  console.log("error")
-              }
-          });
+    getGistData(gistId) {
+      var self = this;
+      if (this.file.length > 0) {
+        data.file = this.file;
       }
-  },
-}
+      $.ajax({
+        url: gistUrl + gistId + ".json",
+        data: data,
+        dataType: "jsonp",
+        timeout: 20000,
+        success: function(response) {
+          self.gistData = response.div;
+        },
+        error: function(response) {
+          console.log("error");
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-@import url('https://assets-cdn.github.com/assets/gist-embed-1baaff35daab552f019ad459494450f1.css');
+@import url("https://github.githubassets.com/assets/gist-embed-4ac6018bcc05457cde2f66d2e7299d11.css");
 </style>
--- src/components/VueGistCore.vue
+++ src/components/VueGistCore.vue
@@ -6,13 +6,13 @@
 <script>
 export default {
   props: {
-      gistDiv: {
-          type: String,
-          required: true
-      }
-  },
-}
+    gistDiv: {
+      type: String,
+      required: true
+    }
+  }
+};
 </script>
 <style scoped>
-@import url('https://assets-cdn.github.com/assets/gist-embed-1baaff35daab552f019ad459494450f1.css');
+@import url("https://github.githubassets.com/assets/gist-embed-4ac6018bcc05457cde2f66d2e7299d11.css");
 </style>
