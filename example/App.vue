289e91a67c4f05f151d9ca4ead6eed2a97f7d0e7
Saravanan Kalimuthu <sarvaruna@outlook.com>
2020-09-27 13:35:35 +0530

    Changes done in example as per the feedback on review (1. V-Bind added in prop gist-id. 2. Css removed for the Buton).

--- example/App.vue
+++ example/App.vue
@@ -26,13 +26,12 @@
       file="index.js"/>
     <br>
     <h3>Change the gist id dynamically</h3>
-    <button 
-      class="button" 
+    <button  
       @click="changeGistId">
       <span>Click Me</span>
     </button>
     <vue-embed-gist 
-      gist-id="gistIdValue"/>
+      :gist-id="gistIdValue"/>
     
   </div>
 </template>
@@ -69,50 +68,4 @@ export default {
   text-align: center;
   margin: 0 auto; 
 }
-
-body .button {
-  width: 400px;
-  height: 100px;
-  background: #f3f0f1;
-  position: relative;
-  background: #f3f0f1;
-  margin-bottom: 25px;
-  border-radius: 32px;
-  text-align: center;
-  cursor: pointer;
-  transition: all 0.1s ease-in-out;
-}
-body .button span {
-  line-height: 100px;
-  font-family: "Montserrat", sans-serif;
-  font-size: 32px;
-  font-weight: semibold;
-}
-body .button:nth-child(1) {
-  box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8), 6px 6px 10px rgba(0, 0, 0, 0.2);
-  color: #6f6cde;
-}
-body .button:nth-child(1):hover {
-  opacity: 0.3;
-  box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8), 6px 6px 10px rgba(0, 0, 0, 0.2);
-}
-body .button:nth-child(1):active {
-  opacity: 1;
-  box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5), inset 8px 8px 16px rgba(0, 0, 0, 0.1);
-  color: #79e3b6;
-}
-body .button:nth-child(2) {
-  opacity: 0.3;
-  box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8), 6px 6px 10px rgba(0, 0, 0, 0.2);
-  color: #6f6cde;
-}
-body .button:nth-child(3) {
-  box-shadow: inset -4px -4px 8px rgba(255, 255, 255, 0.5), inset 8px 8px 16px rgba(0, 0, 0, 0.1);
-  color: #79e3b6;
-}
-body .button:nth-child(3):hover {
-  opacity: 1;
-  box-shadow: -6px -6px 10px rgba(255, 255, 255, 0.8), 6px 6px 10px rgba(0, 0, 0, 0.2);
-}
-
 </style>
